5798743e2fa4ba55bad707ebae39ad752989aab0
Esther Gonzalez <estherdelcarmengonzalez30@gmail.com>
2024-09-06 19:24:14 -0600

    primer commit de intro-ts

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,19 @@
+var num1=38
+let num2=29
+const num3=10
+
+let sum=num1+num2+num3;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+nombre='mario'
+num=32
+activo=true
+
+let matricula:number|string|boolean
+matricula=21000610
+matricula='21000610'
+
+console.log("El numero es", sum);
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,13 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email:string;
+    calificacion?:number;
+}
+
+
+const aumno:Alumno={
+    nombre:'mario',
+    edad:23,
+    email:'mario@gmail.com',
+}
